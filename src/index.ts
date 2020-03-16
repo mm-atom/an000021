896ea8fd07b4495b1aa4084b00d37c7218255af8
taoqf <tao_qf@163.com>
2020-03-16 14:25:54 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
+import { createCanvas, loadImage } from 'canvas';
+import { toCanvas } from 'qrcode';
 
-export default function an21(param1: string, param2: string) {
+/**
+ * 生成带logo的二维码
+ * @param data_for_qrcode	二维码的数据
+ * @param logo Logo
+ * @param width				二维码的宽
+ * @param cwidth			local的宽
+ * @param dark_color		二维码的颜色
+ * @param light_color		背景色
+ */
+export default async function create_qr(data_for_qrcode: string, logo: string, width: number, cwidth: number, dark_color: string, light_color: string) {
+	const cvs = createCanvas(1, 1);
+	const url = await toCanvas(cvs, data_for_qrcode, {
+		color: {
+			dark: dark_color,	// black pixels
+			light: light_color	// white background
+		},
+		errorCorrectionLevel: 'H',	// LMQH
+		margin: 1,
+		width
+	});
+	const canvas = createCanvas(width, width);
+	const img = await loadImage(logo);
+	const ctx = canvas.getContext('2d');
+	ctx.drawImage(url, 0, 0, width, width);
+	const center = (width - cwidth) / 2;
+	ctx.drawImage(img, center, center, cwidth, cwidth);
+	return canvas.toDataURL('image/png');
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,17 @@
 const test = require('ava');
+const { promises } = require('fs');
 
 const { default: a } = require('../dist/index');
+const { readFile } = promises;
 
-test('xxx', (t) => {
-	t.pass();
+test('qrcode', async (t) => {
+	const data_for_qrcode = 'https://mm-edu.gitee.io';		// 二维码的数据
+	const logo = 'https://portrait.gitee.com/uploads/avatars/user/520/1560762_taoqf_1578954569.png!avatar200';
+	const width = 200;				// 二维码的宽
+	const cwidth = 50;				// 二维码的宽
+	const dark_color = '#000000';	// 二维码的颜色
+	const light_color = '#0000ff';	// 背景色
+	const r = await a(data_for_qrcode, logo, width, cwidth, dark_color, light_color);
+	const f = await readFile('./tests/test.png');
+	t.is(f.toString('base64'), r.replace(/.*,/, ''))
 });
